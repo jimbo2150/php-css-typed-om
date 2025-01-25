864769b1029362c5f311c40ea7fbbf20ecc27ac4
Jim Robinson <jimbo2150@gmail.com>
2025-01-25 09:23:15 -0500

    - Removed debug

--- tests/Css/CSSPropertyTest.php
+++ tests/Css/CSSPropertyTest.php
@@ -1,18 +1,19 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Jimbo2150\PhpCssTypedOm\Tests\Css;
 
 use Jimbo2150\PhpCssTypedOm\Css\CSSProperty;
 use PHPUnit\Framework\TestCase;
-use stdClass;
 
 final class CSSPropertyTest extends TestCase
 {
-    public function testCSSPropertyInitialization(): void
-    {
-        $propertiesObj = CSSProperty::getProperties();
+	public function testCSSPropertyInitialization(): void
+	{
+		$propertiesObj = CSSProperty::getProperties();
 		$objProperties = array_keys(get_object_vars($propertiesObj));
-		$this->assertTrue($propertiesObj instanceof stdClass);
-		$this->assertNotEmpty($objProperties, "No CSS properties.");
-    }
+		$this->assertTrue($propertiesObj instanceof \stdClass);
+		$this->assertNotEmpty($objProperties, 'No CSS properties.');
+	}
 }
